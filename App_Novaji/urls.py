842f9ca855112b0ceba80403e12a7a64c1d7b72e
gbolahan <thegbolahanalaba@gmail.com>
2024-10-24 11:18:14 +0100

    Views, serializers and others added

--- App_Novaji/urls.py
+++ App_Novaji/urls.py
@@ -9,3 +9,5 @@ urlpatterns = [
    path('update-registration/<str:reg_id>/', RegisterViewSets.as_view({"put": "update_registration"}), name='registration-update')
 
 ]
+
+"""write a python program that encrypt and decrpyt a text string "Welcome to Lagos" using the AES/256/CBC/PKCS7Padding algorithm from the cryptography library"""
